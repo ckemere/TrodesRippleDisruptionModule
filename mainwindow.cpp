04de3b21e15ff8e0063262993d054970a767c501
Caleb Kemere <ckemere@gmail.com>
2023-07-25 09:59:29 -0500

    Initial commit of GUI

---
+++ mainwindow.cpp
@@ -0,0 +1,25 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->rejectionParamsGroupBox->setVisible(false);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_checkBox_4_stateChanged(int state)
+{
+    if (state)
+        ui->rejectionParamsGroupBox->setVisible(true);
+    else
+        ui->rejectionParamsGroupBox->setVisible(false);
+}
+
